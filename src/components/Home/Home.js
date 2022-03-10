690d4e8edba76bb6dd90baae4267ade5a49d09b1
m-ghiringhelli <mghiring@gmail.com>
2022-03-09 17:31:30 -0800

    setup layout in css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import './Home.css';
+
 import Character from '../Character/Character';
+import Selector from '../Selector/Selector';
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
       Home
       <Character />
+      <Selector />
     </div>
   );
 }
---
+++ src/components/Selector/Selector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Selector.css';
+
+export default function Selector() {
+  return (
+    <div className='selector'>Selector</div>
+  );
+}
