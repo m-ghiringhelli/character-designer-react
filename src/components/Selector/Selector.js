a61dc389a7b82a580cbfe3d0e04f6bcf9436f5bd
m-ghiringhelli <mghiring@gmail.com>
2022-03-09 17:46:03 -0800

    created selects

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -4,6 +4,28 @@ import './Selector.css';
 
 export default function Selector() {
   return (
-    <div className='selector'>Selector</div>
+    <div className='selector'>
+      <select name="head">
+        <option selected disabled>- pick a head -</option>
+        <option value="bird">Bird</option>
+        <option value="cat">Cat</option>
+        <option value="dog">Dog</option>
+        <option value="person">Person</option>
+      </select>
+      <select name="body">
+        <option selected disabled>- pick a top -</option>
+        <option value="jacket">Jacket</option>
+        <option value="suit">Suit</option>
+        <option value="shirt">Shirt</option>
+        <option value="vest">Vest</option>
+      </select>
+      <select name="legs">
+        <option selected disabled>- pick some pants -</option>
+        <option value="pants">Pants</option>
+        <option value="shorts">Shorts</option>
+        <option value="trunks">Trunks</option>
+        <option value="jeans">Jeans</option>
+      </select>
+    </div>
   );
 }
