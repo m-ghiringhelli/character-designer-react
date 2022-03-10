6c4c57ca47e78d596678f3ae14f373d323996697
m-ghiringhelli <mghiring@gmail.com>
2022-03-09 17:12:21 -0800

    created sample character render

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function Character() {
   return (
-    <div>Character</div>
+    <div className='character'>
+      Character
+      <img src="./images/bird-head.jpg" />
+      <img src="./images/jacket-body.jpg" />
+      <img src="./images/shorts-legs.jpg" />
+    </div>
   );
 }
