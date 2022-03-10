c498a3d66176119c3df58fa2590471d73dc7cc04
m-ghiringhelli <mghiring@gmail.com>
2022-03-10 14:58:14 -0800

    adjusted styling

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -5,9 +5,9 @@ export default function Character({ head, body, legs }) {
   return (
     <div className='character'>
       Character
-      <img className='head' src={`./images/${head}-head.jpg`} />
-      <img className='body' src={`./images/${body}-body.jpg`} />
-      <img className='legs' src={`./images/${legs}-legs.jpg`} />
+      <img id='head' src={`./images/${head}-head.jpg`} />
+      <img id='body' src={`./images/${body}-body.jpg`} />
+      <img id='legs' src={`./images/${legs}-legs.jpg`} />
     </div>
   );
 }
