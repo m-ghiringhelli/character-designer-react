82b0d7bf5e4883252e1a67be37469555b8c05011
m-ghiringhelli <mghiring@gmail.com>
2022-03-09 17:55:03 -0800

    added slogan to home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,19 @@ import './Home.css';
 
 import Character from '../Character/Character';
 import Selector from '../Selector/Selector';
+import Slogan from '../Slogan/Slogan';
 
 export default function Home() {
   return (
     <div className='home'>
       Home
-      <Character />
-      <Selector />
+      <div className='character-side'>
+        <Character />
+      </div>
+      <div className='select-side'>
+        <Selector />
+        <Slogan />
+      </div>
     </div>
   );
 }
---
+++ src/components/Slogan/Slogan.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Slogan.css';
+
+export default function Slogan() {
+  return (
+    <div>Slogan</div>
+  );
+}
